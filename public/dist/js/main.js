c877a8deb142d1b6590b24300a64b5629614fa6e
georgezen <georgeutm2014@gmail.com>
2021-07-15 01:23:38 -0500

    logica de paginacion funcional

--- public/dist/js/main.js
+++ public/dist/js/main.js
@@ -1,68 +1,64 @@
-'use strict';
+"use strict";
 
-
-
-const nombre = document.querySelector('#nombre');
-const apellido = document.querySelector('#apellidos');
-const save = document.querySelector('#save');
+const nombre = document.querySelector("#nombre");
+const apellido = document.querySelector("#apellidos");
+const save = document.querySelector("#save");
 
 window.onload = function () {
-    init();    
-}
+    init();
+};
 
 function init() {
-    get_data();    
+    get_data();
 }
 
-function get_data(){
-    let fila ;
-    
+function get_data() {
+    let fila;
+    let page = 1;
+    page--;
+    document.querySelector(".body").innerHTML = "";
+
     fetch("http://localhost/Proyect_laravel/public/get_user")
-    .then(response => response.json())
-    .then(json => {
-        
-        json.map(function(item){
-            fila = `<tr>
-                <td>${item.nombre}</td>
-                <td>${item.id_usuario}</td>
-            </tr>`;
-            console.log(item.nombre);
-            
-            document.querySelector('.body').innerHTML += fila;
+        .then((response) => response.json())
+        .then((json) => {
+            let start = 2 * page;
+            let end = start + 2;
+            let paginados = json.slice(start, end);
+            console.log(paginados.length);
+            console.log(end);
+
+            for (let i = 0; i < paginados.length; i++) {
+                fila = `<tr>
+            <td>${paginados[i].id_usuario}</td>
+            <td>${paginados[i].nombre}</td>
+        </tr>`;
+
+                document.querySelector(".body").innerHTML += fila;
+            }
+
 
         });
-        
-    });
 }
 
 var header = {
-    'Content-Type': 'application/json',
-    'X-CSRF-TOKEN':document.querySelector('#tokenazo').getAttribute("content")
-}
+    "Content-Type": "application/json",
+    "X-CSRF-TOKEN": document.querySelector("#tokenazo").getAttribute("content"),
+};
 
-
-
-
-save.addEventListener('click', () => {
-   
-
+save.addEventListener("click", () => {
     var data = {
         nombre: nombre.value,
-        apellido: apellido.value
-        
+        apellido: apellido.value,
     };
 
-    fetch("http://localhost/Proyect_laravel/public/save",{
-        method: 'POST',
+    fetch("http://localhost/Proyect_laravel/public/save", {
+        method: "POST",
         body: JSON.stringify(data),
         headers: header,
-        mode: 'cors'
+        mode: "cors",
     })
-    .then(response => response.json())
-    .then(json => {
-        console.log(json);
-    });
-
-    
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+        });
 });
-
--- resources/views/plantilla/index.blade.php
+++ resources/views/plantilla/index.blade.php
@@ -19,6 +19,13 @@
         <div class="col-md-12">
             @include('plantilla.table')
         </div>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+              
+              <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+          </nav>
        
 
         
